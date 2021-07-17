d4b0a52d0e96549a2e2e387298c69b012ae772e5
Md Shahil Ahmed <54341137+shahilahmed@users.noreply.github.com>
2021-07-17 16:22:52 +0530

    Add files via upload

--- Core/Lexer.py
+++ Core/Lexer.py
@@ -1,165 +1,1 @@
-import copy
-
-
-class Lexer:	
-	
-	def __init__(self,source = ""):
-		self.source = copy.deepcopy(source) + '\n'
-		self.pos    = 0
-		
-	def is_eof(self):
-		return False if self.pos < len(self.source) else True
-		
-	def next(self):
-		if not self.is_eof():
-			self.pos = self.pos + 1
-		else:
-			self.pos = None
-			
-	def char(self):
-		if not self.is_eof():
-			return self.source[self.pos]
-		else:
-			return None
-	
-	def is_digit(self):
-		if self.char() >= '0' and self.char() <= '9':
-			return True
-		else:
-			return False
-	
-	def is_identifier(self):
-		if self.char() >= 'A' and self.char() <= 'Z':
-			return True
-		elif self.char() >= 'a' and self.char() <= 'z':
-			return True
-		elif self.char() == '_':
-			return True
-		else:
-			return False
-	
-	def is_ws(self):
-		if self.char() == ' ':
-			return True
-		elif self.char() == '\n':
-			return True
-		elif self.char() == '\r':
-			return True
-		elif self.char() == '\t':
-			return True
-		else:
-			return False
-	
-	def read_comment(self):
-		self.next()
-		if self.char() == '#':
-			self.next()
-			while self.char() != '\n':
-				if self.is_eof():
-					break
-				self.next()
-		elif self.char() == '*':
-			self.next()
-			while self.char() != '*':
-				if self.is_eof():
-					break
-				self.next()
-			self.next()
-			if self.char() == '#':
-				self.next()
-	'''
-	def read_string(self):
-		str_data = ""
-		self.next()
-		while self.char() not in ["\""]:
-			if self.is_eof():
-				break
-			if self.char() in ["\\"]:
-				self.next()
-				str_data = str_data + self.char()
-				self.next()
-			str_data = str_data + self.char()
-			self.next()
-		self.next()
-		str_data = "\"{}\"".format(str_data)
-		return str_data
-	'''
-	
-	def read_number(self):
-		str_data = ""
-		while self.is_digit():
-			if self.is_eof():
-				break
-			str_data = str_data + self.char()
-			self.next()
-		return int(str_data)
-	
-	def read_identifier(self):
-		str_data = ""
-		while self.is_identifier() or self.is_digit():
-			if self.is_eof():
-				break
-			str_data = str_data + self.char()
-			self.next()
-		return str_data
-		
-	def tokenize(self):
-		result = []
-		while not self.is_eof():
-			if self.is_ws():
-				self.next()
-			elif self.char() in ['#']:
-				self.read_comment()
-			elif self.char() in ['[','{','(',')','}',']',';',',']:
-				str_data = self.char()
-				result.append(str_data)
-				self.next()
-			elif self.char() in ['+','-','*','%','/','^','<','>','=','!','&','|']:
-				str_data = self.char()
-				if self.char() in ['=']:
-					self.next()
-					if self.char() in ['=']:
-						str_data = str_data + self.char()
-						self.next()
-				elif self.char() in ['!']:
-					self.next()
-					if self.char() in ['=']:
-						str_data = str_data + self.char()
-						self.next()
-				elif self.char() in ['<']:
-					self.next()
-					if self.char() in ['=']:
-						str_data = str_data + self.char()
-						self.next()
-				elif self.char() in ['>']:
-					self.next()
-					if self.char() in ['=']:
-						str_data = str_data + self.char()
-						self.next()
-				elif self.char() in ['&']:
-					self.next()
-					if self.char() in ['&']:
-						str_data = str_data + self.char()
-						self.next()
-				elif self.char() in ['|']:
-					self.next()
-					if self.char() in ['|']:
-						str_data = str_data + self.char()
-						self.next()
-				else:
-					self.next()
-				result.append(str_data)
-				'''
-				Shift + Tab
-				Tab out ward <<<----
-				elif self.char() in ["\""]:
-					result.append(self.read_string())
-				'''
-			elif self.is_digit():
-				result.append(self.read_number())
-			elif self.is_identifier():
-				result.append(self.read_identifier())
-			else:
-				raise Exception("Unknown Character: {}".format(self.char()))
-		result.append('eof')
-		return result
+import copyclass Lexer:			def __init__(self,source = ""):		self.source = copy.deepcopy(source) + '\n'		self.pos    = 0			def is_eof(self):		return False if self.pos < len(self.source) else True			def next(self):		if not self.is_eof():			self.pos = self.pos + 1		else:			self.pos = None				def char(self):		if not self.is_eof():			return self.source[self.pos]		else:			return None		def is_digit(self):		if self.char() >= '0' and self.char() <= '9':			return True		else:			return False		def is_identifier(self):		if self.char() >= 'A' and self.char() <= 'Z':			return True		elif self.char() >= 'a' and self.char() <= 'z':			return True		elif self.char() == '_':			return True		else:			return False		def is_ws(self):		if self.char() == ' ':			return True		elif self.char() == '\n':			return True		elif self.char() == '\r':			return True		elif self.char() == '\t':			return True		else:			return False		def read_comment(self):		self.next()		if self.char() == '#':			self.next()			while self.char() != '\n':				if self.is_eof():					break				self.next()		elif self.char() == '*':			self.next()			while self.char() != '*':				if self.is_eof():					break				self.next()			self.next()			if self.char() == '#':				self.next()	'''	def read_string(self):		str_data = ""		self.next()		while self.char() not in ["\""]:			if self.is_eof():				break			if self.char() in ["\\"]:				self.next()				str_data = str_data + self.char()				self.next()			str_data = str_data + self.char()			self.next()		self.next()		str_data = "\"{}\"".format(str_data)		return str_data	'''		def read_number(self):		str_data = ""		while self.is_digit():			if self.is_eof():				break			str_data = str_data + self.char()			self.next()		return int(str_data)		def read_identifier(self):		str_data = ""		while self.is_identifier() or self.is_digit():			if self.is_eof():				break			str_data = str_data + self.char()			self.next()		return str_data			def tokenize(self):		result = []		while not self.is_eof():			if self.is_ws():				self.next()			elif self.char() in ['#']:				self.read_comment()			elif self.char() in ['[','{','(',')','}',']',';',',']:				str_data = self.char()				result.append(str_data)				self.next()			elif self.char() in ['+','-','*','%','/','^','<','>','=','!','&','|']:				str_data = self.char()				if self.char() in ['=']:					self.next()					if self.char() in ['=']:						str_data = str_data + self.char()						self.next()				elif self.char() in ['!']:					self.next()					if self.char() in ['=']:						str_data = str_data + self.char()						self.next()				elif self.char() in ['<']:					self.next()					if self.char() in ['=']:						str_data = str_data + self.char()						self.next()				elif self.char() in ['>']:					self.next()					if self.char() in ['=']:						str_data = str_data + self.char()						self.next()				elif self.char() in ['&']:					self.next()					if self.char() in ['&']:						str_data = str_data + self.char()						self.next()				elif self.char() in ['|']:					self.next()					if self.char() in ['|']:						str_data = str_data + self.char()						self.next()				else:					self.next()				result.append(str_data)				'''				Shift + Tab				Tab out ward <<<----				elif self.char() in ["\""]:					result.append(self.read_string())				'''			elif self.is_digit():				result.append(self.read_number())			elif self.is_identifier():				result.append(self.read_identifier())			else:				raise Exception("Unknown Character: {}".format(self.char()))		result.append('eof')		return result
--- Core/Parser.py
+++ Core/Parser.py
@@ -1,94 +1,1 @@
-import copy
-
-
-class Parser:	
-	
-	def __init__(self,tokens = []):
-		self.tokens = copy.deepcopy(tokens)
-		self.pos    = 0
-		
-	def is_eof(self):
-		return False if self.pos < len(self.tokens) else True
-		
-	def token(self):
-		if not self.is_eof():
-			return self.tokens[self.pos]
-		else:
-			return None
-	
-	def next(self):
-		if not self.is_eof():
-			self.pos = self.pos + 1
-		else:
-			self.pos = None			
-
-	def match(self,value = ""):
-		if self.token() == value:
-			self.next()
-		else:
-			raise Exception("Parse Error: Excepted: {} but got {}".format(value,self.token()))
-	
-	def error(self,where = ''):
-		raise Exception("Parse Error at {}.".format(where))
-	
-	''' atom = number | identifier | ( exp ) '''
-	def parse_atom(self):
-		result = None
-		token = self.token()
-		if isinstance(token,int) or isinstance(token,float):
-			result = token
-			self.next()
-		elif token == '-':
-			token = self.token()
-			self.match('-')
-			result = [token,self.parse_atom()]
-		elif token == '(':
-			self.match('(')
-			result = self.parse_exp()
-			self.match(')')
-		elif isinstance(token,str):
-			result = token
-			self.next()
-		else:
-			self.error("parse_atom Unknown Token: {}".format(token))
-		return result
-		
-	''' term -> atom ((* | / | %) atom)* '''
-	def parse_term(self):
-		result = self.parse_atom()
-		while self.token() in ['*','/','%']:
-			token = self.token()
-			self.match(token)
-			result = [token,result,self.parse_atom()]
-		return result
-	
-	''' exp -> term ((+ | -) term)* '''
-	def parse_exp(self):
-		result = self.parse_term()
-		while self.token() in ['+','-']:
-			token = self.token()
-			self.match(token)
-			result = [token,result,self.parse_term()]
-		return result
-	
-	''' statement -> let identifier = exp | exp'''
-	def parse_statement(self):
-		result = None
-		if self.token() == "let":
-			self.match("let")
-			token = self.token()
-			if isinstance(self.token(),str):
-				variable = token
-				self.next()
-				self.match("=")
-				result = ["let",variable,self.parse_exp()]
-			else:
-				raise Exception("Required a identifier of type string.")
-		else:
-			result = self.parse_exp()
-		return result
-		
-	def parse(self):
-		result = self.parse_statement()
-		self.match("eof")
-		return result
+import copyclass Parser:			def __init__(self,tokens = []):		self.tokens = copy.deepcopy(tokens)		self.pos    = 0			def is_eof(self):		return False if self.pos < len(self.tokens) else True			def token(self):		if not self.is_eof():			return self.tokens[self.pos]		else:			return None		def next(self):		if not self.is_eof():			self.pos = self.pos + 1		else:			self.pos = None				def match(self,value = ""):		if self.token() == value:			self.next()		else:			raise Exception("Parse Error: Excepted: {} but got {}".format(value,self.token()))		def error(self,where = ''):		raise Exception("Parse Error at {}.".format(where))		''' atom = number | identifier | ( exp ) '''	def parse_atom(self):		result = None		token = self.token()		if isinstance(token,int) or isinstance(token,float):			result = token			self.next()		elif token == '-':			token = self.token()			self.match('-')			result = [token,self.parse_atom()]		elif token == '(':			self.match('(')			result = self.parse_exp()			self.match(')')		elif isinstance(token,str):			result = token			self.next()		else:			self.error("parse_atom Unknown Token: {}".format(token))		return result			''' expo -> atom ((^ ) expo)* '''	def parse_expo(self):		result = self.parse_atom()		while self.token() in ['^']:			token = self.token()			self.match(token)			result = [token,result,self.parse_expo()]		return result		''' term -> atom ((* | / | %) atom)* '''	def parse_term(self):		result = self.parse_expo()		while self.token() in ['*','/','%']:			token = self.token()			self.match(token)			result = [token,result,self.parse_expo()]		return result		''' exp -> term ((+ | -) term)* '''	def parse_exp(self):		result = self.parse_term()		while self.token() in ['+','-']:			token = self.token()			self.match(token)			result = [token,result,self.parse_term()]		return result		''' statement -> let identifier = exp | exp'''	def parse_statement(self):		result = None		if self.token() == "let":			self.match("let")			token = self.token()			if isinstance(self.token(),str):				variable = token				self.next()				self.match("=")				result = ["let",variable,self.parse_exp()]			else:				raise Exception("Required a identifier of type string.")		else:			result = self.parse_exp()		return result			''' parse -> statement eof'''	def parse(self):		result = self.parse_statement()		self.match("eof")		return result
